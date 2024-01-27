e15c7774d5ff34d5a622cf58a300d6ed8d871705
Matthew Wilding <mbwilding@gmail.com>
2024-01-27 18:54:44 +0800

    eframe/egui added

---
+++ src/app.rs
@@ -0,0 +1,90 @@
+use crate::windows_manager::WindowsManager;
+use eframe::egui;
+use eframe::emath::Align;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Deserialize, Serialize)]
+#[serde(default)]
+pub struct App {
+    #[serde(skip)]
+    pub windows_manager: WindowsManager,
+
+    settings: Settings,
+
+    #[serde(skip)]
+    window_state: WindowState,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Settings {
+    test: f32,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self { test: 0.0 }
+    }
+}
+
+#[derive(Default)]
+struct WindowState {
+    settings: bool,
+}
+
+impl App {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
+        Default::default()
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                // if !cfg!(target_arch = "wasm32") {
+                //     ui.menu_button("File", |ui| {
+                //         if ui.button("Quit").clicked() {
+                //             ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                //         }
+                //     });
+                //     ui.add_space(16.0);
+                // }
+
+                egui::widgets::global_dark_light_mode_switch(ui);
+
+                // ui.add_space(16.0);
+
+                if ui.button("Settings").clicked() {
+                    self.window_state.settings = !self.window_state.settings;
+                }
+            });
+        });
+
+        egui::Window::new("Settings")
+            .open(&mut self.window_state.settings)
+            .show(ctx, |ui| {
+                let settings = &mut self.settings;
+
+                egui::containers::collapsing_header::CollapsingHeader::new("Parameters")
+                    .default_open(true)
+                    .show(ui, |ui| {
+                        ui.horizontal(|ui| {
+                            ui.label("Test");
+                            ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
+                                ui.add(egui::Slider::new(&mut settings.test, 0.0..=600.0));
+                            });
+                        });
+                    });
+            });
+
+        egui::CentralPanel::default().show(ctx, |_ui| {});
+    }
+
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod app;
 mod single;
 mod structs;
 mod win32_helpers;
@@ -8,18 +9,34 @@ mod windows_manager;
 mod windows_window;
 
 use crate::windows_manager::WindowsManager;
-use anyhow::Result;
+use eframe::egui;
 
 pub const APP_NAME: &str = "Tile-RS";
 
-fn main() -> Result<()> {
+fn main() -> eframe::Result<()> {
     env_logger::init();
-    single::check()?;
-    let windows_manager = WindowsManager::default();
-    windows_manager.init();
+    single::check();
 
-    // TODO: Remove this.
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(u64::MAX));
-    }
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1280.0, 720.0])
+            .with_min_inner_size([300.0, 220.0])
+            .with_icon(
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                    .unwrap(),
+            ),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        APP_NAME,
+        native_options,
+        Box::new(move |cc| {
+            let mut app = app::App::new(cc);
+            let windows_manager = WindowsManager::default();
+            windows_manager.init();
+            app.windows_manager = windows_manager;
+            Box::new(app::App::new(cc))
+        }),
+    )
 }
